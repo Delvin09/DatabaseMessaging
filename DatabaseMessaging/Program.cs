4cb522709258fb49aebe492731091c560ec48d62
philimonovoleg <philimonovoleg@gmail.com>
2019-09-27 23:24:02 +0300

    Add project files.

---
+++ DatabaseMessaging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseMessaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
